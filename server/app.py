44a54f585f5ad74a824ebe9ee85a04901a4b2ba4
Reshika <reshikapv@gmail.com>
2020-08-01 19:55:02 +0800

    add server folder

--- server/app.py
+++ server/app.py
@@ -11,18 +11,8 @@ cors = CORS(app)
 
 # sanity check route
 @app.route("/")
-def home():
+def get_allData():
     return "Hello, World!"
-
-# @app.route("/data", methods=['POST'])
-# def load_data():
-#     filter_data = request.get_json()
-#     print(filter_data)
-#     Pollutant = filter_data['pollutants']
-#     Date_time = filter_data['Date']
-#     Future_hour = filter_data['Future_hour']
-#     Method = filter_data['Method']
-#     return dataprocessing.map_data(Pollutant,Future_hour,Method,Date_time)
 
 @app.route("/data", methods=['POST'])
 def load_data():
@@ -35,19 +25,14 @@ def load_data():
 @app.route("/lineChart1", methods=['POST'])
 def lineChart1():
     data = request.get_json()
-    print(data)
     return dataprocessing.data_lineChart_1(data)
 
-@app.route("/lineChart2", methods=['POST'])
-def lineChart2():
-    data = request.get_json()
-    onsite_code = data['Onsite_code']
-    return dataprocessing.data_lineChart_2(onsite_code)
+# @app.route("/lineChart2", methods=['POST'])
+# def lineChart2():
+#     data = request.get_json()
+#     onsite_code = data['Onsite_code']
+#     return dataprocessing.data_lineChart_2(onsite_code)
 
-@app.route("/getMetrics", methods=['POST'])
-def getMetrics():
-    data = request.get_json()
-    return dataprocessing.metrics(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
